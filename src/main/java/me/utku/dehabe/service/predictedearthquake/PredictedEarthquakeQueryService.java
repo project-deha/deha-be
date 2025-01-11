c40fe54ea5158931b1b2569fca32b4ff8575e603
utkuonursahin <sahinutkuonur@gmail.com>
2025-01-11 20:52:39 +0300

    refactor(predictedEarthquake): create TurkishCity enum and refactor default parameter assignments

--- src/main/java/me/utku/dehabe/service/predictedearthquake/PredictedEarthquakeQueryService.java
+++ src/main/java/me/utku/dehabe/service/predictedearthquake/PredictedEarthquakeQueryService.java
@@ -2,6 +2,7 @@ package me.utku.dehabe.service.predictedearthquake;
 
 import me.utku.dehabe.dto.predictedearthquake.PredictedEarthquakeDto;
 import me.utku.dehabe.dto.predictedearthquake.PredictedEarthquakeFilterDto;
+import me.utku.dehabe.enums.TurkishCity;
 import me.utku.dehabe.mapper.PredictedEarthquakeMapper;
 import me.utku.dehabe.model.earthquakes.PredictedEarthquake;
 import me.utku.dehabe.repository.PredictedEarthquakeRepository;
@@ -10,11 +11,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PredictedEarthquakeQueryService {
@@ -32,53 +31,21 @@ public class PredictedEarthquakeQueryService {
     }
 
     public Page<PredictedEarthquake> getAllFiltered(PredictedEarthquakeFilterDto filterDto) {
-        Instant startDate = Optional.ofNullable(filterDto.startDate()).orElse(Instant.now()
-                .minus(5, ChronoUnit.DAYS));
-        Instant endDate = Optional.ofNullable(filterDto.startDate())
-                .orElse(Instant.now())
-                .atZone(ZoneId.systemDefault())
-                .plusMonths(12)
-                .toInstant();
-
+        Instant startDate = filterDto.startDate() == null ? Instant.now() : filterDto.startDate();
+        Instant endDate = filterDto.endDate() == null ? Instant.now().plus(365, ChronoUnit.DAYS) : filterDto.endDate();
         float minMagnitude = filterDto.minMagnitude() == 0F ? 1F : filterDto.minMagnitude();
         float maxMagnitude = filterDto.maxMagnitude() == 0F ? 10F : filterDto.maxMagnitude();
+        List<String> cityList = filterDto.city() == null ?
+                Arrays.stream(TurkishCity.values())
+                        .map(TurkishCity::getName)
+                        .toList() : List.of(filterDto.city());
 
-
-        List<String> CITIES = Arrays.asList(
-                "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Amasya",
-                "Ankara", "Antalya", "Artvin", "Aydın", "Balıkesir",
-                "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur",
-                "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli",
-                "Diyarbakır", "Edirne", "Elazığ", "Erzincan", "Erzurum",
-                "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari",
-                "Hatay", "Isparta", "Mersin", "İstanbul", "İzmir",
-                "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir",
-                "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa",
-                "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir",
-                "Niğde", "Ordu", "Rize", "Sakarya", "Samsun",
-                "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat",
-                "Trabzon", "Tunceli", "Şanlıurfa", "Uşak", "Van",
-                "Yozgat", "Zonguldak", "Aksaray", "Bayburt", "Karaman",
-                "Kırıkkale", "Batman", "Şırnak", "Bartın", "Ardahan",
-                "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye",
-                "Düzce"
-        );
-
-        List<String> cityList;
-
-        if (filterDto.city() == null) {
-            cityList = CITIES;
-        } else {
-            cityList = Arrays.asList(filterDto.city());
-        }
-
-        Page<PredictedEarthquake> predictions = predictedEarthquakeRepository
+        return predictedEarthquakeRepository
                 .findAllByMagnitudeIsBetweenAndLocation_CityInAndPredictionDateIsBetween(
                         minMagnitude, maxMagnitude,
                         cityList,
                         startDate, endDate,
                         PageRequest.of(filterDto.page(), filterDto.size())
                 );
-        return predictions;
     }
 }
